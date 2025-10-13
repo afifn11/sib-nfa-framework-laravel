2043db1dbb0d3325eec837e6a1f8a8fb6b329d5b
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2025-10-13 13:22:58 +0700

    Tugas Pertemuan 1 - Routing dan MVC

---
+++ pertemuan-1/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all(); // Ambil data dari model
+        return view('authors', compact('authors')); // Kirim ke view
+    }
+}
